a6a1e740b73d7e20b25e73c868805e818c4a6a9a
quadrakill <980539740@qq.com>
2021-06-23 17:12:07 +0800

    fix

---
+++ dist/ap-utils.es5.js
@@ -0,0 +1,8 @@
+var Demo = /** @class */ (function () {
+    function Demo() {
+    }
+    return Demo;
+}());
+
+export default Demo;
+//# sourceMappingURL=ap-utils.es5.js.map
---
+++ dist/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Demo = /** @class */ (function () {
+    function Demo() {
+    }
+    return Demo;
+}());
+exports.default = Demo;
+//# sourceMappingURL=index.js.map
