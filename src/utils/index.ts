3e681f01cbadb403e98ce53ed3a1500b3661fc5b
quadrakill <980539740@qq.com>
2021-07-06 17:30:53 +0800

    feat: utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,11 +6,70 @@
  * @returns
  */
 export function once(fn: Function | null) {
-  return function (...args: any[]) {
+  return function fn1(...args: any[]) {
     if (fn) {
-      let ret = fn.apply(null, args);
+      let ret = fn.apply(fn1, args);
       fn = null;
       return ret;
+    }
+  };
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {Function} fn
+ * @param {{ beforeCall?: Function; afterCall?: Function }} { beforeCall, afterCall }
+ * @returns
+ */
+export function intercept(
+  fn: Function,
+  { beforeCall, afterCall }: { beforeCall?: Function; afterCall?: Function }
+) {
+  return function fn1(...args: any[]) {
+    if (!beforeCall || beforeCall.call(fn1, args) != false) {
+      let ret = fn.apply(fn1, args);
+      if (afterCall) return afterCall.call(fn1, ret);
+      return ret;
+    }
+  };
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {Function} fn
+ * @param {number} wait
+ * @returns
+ */
+export function debounce(fn: Function, wait: number) {
+  let debounceTimer: number;
+  return function fn1(...args: any[]) {
+    if (debounceTimer) window.clearTimeout(debounceTimer);
+    debounceTimer = window.setTimeout(() => {
+      fn.apply(fn1, args);
+    }, wait);
+  };
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {Function} fn
+ * @param {number} wait
+ * @returns
+ */
+export function throttle(fn: Function, wait: number) {
+  let throttleTimer: number;
+  return function fn1(...args: any[]) {
+    if (!throttleTimer) {
+      throttleTimer = window.setTimeout(() => {
+        fn.apply(fn1, args);
+        window.clearTimeout(throttleTimer);
+      }, wait);
     }
   };
 }
