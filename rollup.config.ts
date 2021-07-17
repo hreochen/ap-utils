425d1241bd958904712f3ed6de1493b7596eb4a2
quadrakill <980539740@qq.com>
2021-07-17 09:42:45 +0800

    fix: package.json files

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,30 +1,26 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import camelCase from 'lodash.camelcase'
-import typescript from '@rollup/plugin-typescript'
-import json from '@rollup/plugin-json'
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import camelCase from "lodash.camelcase";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import pkg from "./package.json";
 
-
-const pkg = require('./package.json')
-
-const libraryName = 'ap-utils'
-
+const libraryName = "ap-utils";
 
 export default {
-    input: 'src/index.ts',
-    output: [
-      { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-      { file: pkg.module, format: 'es', sourcemap: true },
-    ],
-    external: [],
-    watch: {
-      include: 'src/**',
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: "umd",
+      sourcemap: true,
     },
-    plugins: [
-      json(),
-      typescript(),
-      commonjs(),
-      resolve()
-    ],
-  }
-  
+    { file: pkg.module, format: "es", sourcemap: true },
+  ],
+  external: [],
+  watch: {
+    include: "src/**",
+  },
+  plugins: [json(), typescript(), commonjs(), resolve()],
+};
